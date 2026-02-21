4e61eebcec79d80d4144708e78be4771a55178cd
manyasingh0108 <manya01singh08@gmail.com>
2026-02-21 10:59:08 +0530

    17/02/26

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import ProductList from './ProductList.jsx'; 
+
+export default function App() {
+  const [products, setproducts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://dummyjson.com/products')
+      .then((response) => response.json())
+      .then((data) => {
+       
+        setproducts(data.products); 
+      })
+      .catch((error) => { 
+         console.error('Error fetching products:', error);
+      });
+  }, []);
+
+  return (
+    <div className="app"> 
+      <center><h2>Product Listing - (2500032608) - Rajnish Kumar Ranjan</h2> </center>
+      {products.length === 0 ? (
+        <p>No products available</p>
+      ) : (
+        <ProductList products={products} />
+      )}
+    </div>
+  );
+}
